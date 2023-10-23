7981ca5d64b383b7243a73c7a67d1e33f7899319
fatihkaratash <f.karatash.dev@gmail.com>
2023-10-23 16:46:11 +0300

    First

---
+++ helloworld.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    
+    /* //Entry1
+    cout << "I like pizza!" << '\n';
+    cout << "It's really good" << endl;
+    */
+    
+    /*Variables2
+
+    int x;  //declaration
+    //int x = 5; 2. kere belirtmek hata
+    x = 5; //assignment
+
+    int y = 8;
+    int sum = x + y;
+
+    cout << x << '\n' << y << endl;
+    cout << sum << endl;
+    
+    char grade = 'F';
+    double initial = 25.4;
+    bool isStudent = true;
+    bool forSale = true;
+    string name = "Bro";
+
+
+    cout << name << endl << "hello \n";
+    cout << "You are " << initial << " years old";
+    */
+
+   // CONST3
+   //Const = read only = prevent anything from modifying it
+   const double pi = 3.14159;
+   
+   double radius = 10;
+   double circumference = 2 * pi * radius;
+
+   cout << circumference << "cm" << endl;
+
+   //26:32
+   
+   
+   
+
+
+    return 0;
+}
+
+
+
+
+
+
+    
